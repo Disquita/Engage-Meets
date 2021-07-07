3f90a7157ea524a095dbe9590a73d642b0d91fc5
disquita <cindytiru@gmail.com>
2021-07-08 02:40:46 +0400

    deployed

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+//const socket = io('http://localhost:5000');
+const socket = io('https://engage-meets.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
 
